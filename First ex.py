8e6dfac4d94adb8e250075dc2c9f097443fe3855
Adhamsw90 <Adhamsw90@gmail.com>
2020-11-07 19:55:48 +0200

    Learning python

--- First ex.py
+++ First ex.py
@@ -6,7 +6,7 @@ Meot=5
 Asarot=6
 Ahadot=7
 '''
-
+######This is my solution######
 num=4567
 
 print("My num is:" + str(num))
@@ -24,3 +24,17 @@ print("Asarot is: " +str(Asarot))
 
 Ahadot=((num-(Alafim*1000))-Meot*100)%10
 print("Ahadot is: " +str(Ahadot))
+
+
+#######This is Idan's solution ! ########
+
+
+num1=4567
+
+print("Alafimm " + str(num1//1000))
+
+print("Meoott " +str((num1%1000)//100))
+
+print("Asarott " +str((num1%100)//10))
+
+print("Ahadott " +str(num1%10))
