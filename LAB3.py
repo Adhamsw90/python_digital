96842b89ddf22481721c9579a54e0d07f0b976d7
Adhamsw90 <Adhamsw90@gmail.com>
2020-11-10 21:43:28 +0200

    Learning python

---
+++ LAB3.py
@@ -0,0 +1,13 @@
+Name="Adham"
+Mail="adham@gmail.com"
+Age=30
+
+print("Name is :" +Name + "\nMail is :" + Mail + "\nAge is :" +str(Age))
+
+print("\n\nName is :" +(Name)[::-1] + "\nMail is :" + Mail + "\nAge is : " + str(Age*3))
+
+
+
+a="idan dudu yuval ben gal adam yana shahar"
+
+print(Name in a)
